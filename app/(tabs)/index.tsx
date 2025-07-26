452ac5927700c6a7561f932bb0ad7169e01a2efb
akbarSTUDENT <muhammadakbar15042004@gmail.com>
2025-07-26 23:36:11 +0800

    TUGAS 6 LAB

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,59 +1,36 @@
-import { ScrollView, Text } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Entypo from "@expo/vector-icons/Entypo";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import React from "react";
+import { ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <ScrollView contentContainerStyle={{ padding: 20 }}>
-      <Text
-        style={{ fontFamily: "bitcount-medium", fontSize: 26, color: "blue" }}
+    <ScrollView contentContainerStyle={{ padding: 20, flexGrow: 1 }}>
+      <Text style={{ fontSize: 100, marginBottom: 10 }}>
+        Daftar 10 Ikon Baru:
+      </Text>
+
+      <View
+        style={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
       >
-        ALIF RYANTO RAHMAN
-        {"\n"}105841104222
-      </Text>
-
-      <Text style={{ fontFamily: "bitcount-prop-regular", fontSize: 24 }}>
-        erika yanti
-        {"\n"}105841104322
-      </Text>
-
-      <Text style={{ fontFamily: "bitcount-extrabold", fontSize: 24 }}>
-        Zulkifli
-        {"\n"}105841104422
-      </Text>
-
-      <Text style={{ fontFamily: "bitcount-variable", fontSize: 24 }}>
-        FIFIANA
-        {"\n"}105841104522
-      </Text>
-
-      <Text style={{ fontFamily: "bitcount-prop-variable", fontSize: 24 }}>
-        MUH. AKBAR HAERUDDIN
-        {"\n"}105841104622
-      </Text>
-
-      <Text style={{ fontFamily: "bitcount-single-variable", fontSize: 24 }}>
-        AGUSTIANA
-        {"\n"}105841104722
-      </Text>
-
-      <Text style={{ fontFamily: "inter-variable", fontSize: 24 }}>
-        Dia rahmatillah
-        {"\n"}105841104822
-      </Text>
-
-      <Text style={{ fontFamily: "inter-extrabold", fontSize: 24 }}>
-        Juliani
-        {"\n"}105841104922
-      </Text>
-
-      <Text style={{ fontFamily: "opensans-bold", fontSize: 24 }}>
-        Azzah Aulia Syarif
-        {"\n"}105841105022
-      </Text>
-
-      <Text style={{ fontFamily: "opensans-italic-variable", fontSize: 24 }}>
-        Syauqiyah Mujahidah Yahya
-        {"\n"}105841105122
-      </Text>
+        <AntDesign name="pluscircleo" size={500} color="black" />
+        <AntDesign name="infocirlceo" size={500} color="black" />
+        <Entypo name="controller-play" size={500} color="black" />
+        <Entypo name="clapperboard" size={500} color="black" />
+        <EvilIcons name="location" size={500} color="black" />
+        <EvilIcons name="sc-facebook" size={500} color="black" />
+        <Feather name="camera" size={500} color="black" />
+        <Feather name="moon" size={500} color="black" />
+        <FontAwesome name="music" size={500} color="black" />
+        <FontAwesome name="book" size={500} color="black" />
+      </View>
     </ScrollView>
   );
 }
